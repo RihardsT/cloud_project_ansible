160899ce99b7a91027e8b989aa38014ddf18ca12
Rihards <rudenspavasaris@proton.me>
2023-06-16 19:48:04 +0300

    Fix the Scaleway DNS script

--- files/scaleway_dns.sh
+++ files/scaleway_dns.sh
@@ -1,13 +1,14 @@
-scaleway_token = $1
-node_ip = $2
-
-record_ids=$(/usr/bin/curl -s -X GET -H "X-Auth-Token: ${scaleway_token}" \
-https://api.scaleway.com/domain/v2beta1/dns-zones/rudenspavasaris.id.lv/records | /usr/bin/jq '.records[] | select(.type == "A") | .id')
-for i in "${record_ids[@]}"; do
-  /usr/bin/curl -X PATCH -H 'X-Auth-Token: ${scaleway_token}' -H 'Content-Type: application/json' \
+scaleway_token=${1}
+node_ip=${2}
+record_ids=($(/usr/bin/curl -s -X GET -H "X-Auth-Token: ${scaleway_token}" https://api.scaleway.com/domain/v2beta1/dns-zones/rudenspavasaris.id.lv/records | /usr/bin/jq '.records[] | select(.type == "A") | .id'))
+for i in ${record_ids[*]}; do
+  record_data='{"changes": [{"delete": {"id": '${i}'}}]}'
+  /usr/bin/curl -X PATCH -H "X-Auth-Token: ${scaleway_token}" -H 'Content-Type: application/json' \
   https://api.scaleway.com/domain/v2beta1/dns-zones/rudenspavasaris.id.lv/records -d \
-  "{\"changes\": [{\"delete\": {\"id\": ${i}}}]}"
+  "${record_data}"
 done
+new_record_data='{"changes": [{"add": {"records": [{"name": "","data": "'${node_ip}'","ttl": "600","type": "A"},
+{"name": "*","data": "'${node_ip}'","ttl": "600","type": "A"}]}}]}';
 /usr/bin/curl -X PATCH -H "X-Auth-Token: ${scaleway_token}" -H "Content-Type: application/json" \
 https://api.scaleway.com/domain/v2beta1/dns-zones/rudenspavasaris.id.lv/records -d \
-'{"changes": [{"add": {"records": [{"name": "","data": "${node_ip}","ttl": "600","type": "A"},{"name": "*","data": "${node_ip}","ttl": "600","type": "A"}]}}]}';
+"${new_record_data}"
