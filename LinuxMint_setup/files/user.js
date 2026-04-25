9ac027a7e5e5f14eae5c539b9ce7da91a2ddc052
Rihards <rudenspavasaris@proton.me>
2026-04-25 20:26:32 +0300

    Additional Firefox user_pref for expanded bookmark menu

--- LinuxMint_setup/files/user.js
+++ LinuxMint_setup/files/user.js
@@ -4,4 +4,8 @@ user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
 // Not sure if this will work
 user_pref("browser.search.defaultenginename", "DuckDuckGo"); // DuckDuckGo or Google
 user_pref("browser.search.selectedEngine", "DuckDuckGo"); // DuckDuckGo or Google
+
+// To enable loading custom stylesheet, to expand Edit Bookmarks menu
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// https://www.reddit.com/r/FirefoxCSS/comments/g30phz/expand_all_bookmark_folders_on_new_bookmark_edit/
+user_pref("browser.bookmarks.editDialog.firstEditField", "foldersExpander"); // Default: namePicker
--- OpenSuse_setup/README.md
+++ OpenSuse_setup/README.md
@@ -2,10 +2,10 @@
 ```
 sudo zypper install ansible
 
-cd Code/cloud_project/cloud_project_ansible/OpenSuse_setup/
+cd ~/Code/cloud_project/cloud_project_ansible/OpenSuse_setup/
 sudo echo
 ansible-playbook ./laptop.yaml --diff
-# Use --ask-become-pass, as the sudo echo thing stopped working
+# Use --ask-become-pass/-K, as the sudo echo thing stopped working
 ansible-playbook ./laptop.yaml --diff --ask-become-pass
 
 sudo echo
